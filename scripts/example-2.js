da65cd917031e6456a22b1d4685bf1e6f5d67cba
dvdrtrgn <dvdrtrgn@gmail.com>
2014-10-24 15:08:32 -0500

    add slider to banner

--- scripts/example-2.js
+++ scripts/example-2.js
@@ -1,2 +1,35 @@
-var y = 2;
-y++;
+/*jslint white:false */
+/*globals $, initTabs:true */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+$(function () {
+    var banner = $('#slider');
+
+    if (banner.length) {
+        banner.nivoSlider({
+            effect: 'random',
+            slices: 15,
+            boxCols: 8,
+            boxRows: 4,
+            animSpeed: 500,
+            pauseTime: 10000,
+            startSlide: 0,
+            directionNav: true,
+            directionNavHide: true,
+            controlNav: false,
+            controlNavThumbs: false,
+            controlNavThumbsFromRel: false,
+            controlNavThumbsSearch: '.jpg',
+            controlNavThumbsReplace: '_thumb.jpg',
+            keyboardNav: true,
+            pauseOnHover: true,
+            manualAdvance: false,
+            captionOpacity: 0.8,
+            prevText: '«',
+            nextText: '»',
+            randomStart: false,
+            z: 0
+        });
+    }
+});
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
